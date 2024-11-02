cd01157eaa08516dcd66c42135f435495af62490
Moth <tdrice668@gmail.com>
2024-11-02 18:10:08 -0500

    added pages folder with a file for each page, adjusted components to match assignment requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 
-function App() {
-  let items = ["prework-study-guide", "markdown-master", "towmato", "workforce-wiz", "weather", "Color-My-World"];
+// function App() {
+//   let items = ["prework-study-guide", "markdown-master", "towmato", "workforce-wiz", "weather", "Color-My-World"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-  // each component has individual state
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   // each component has individual state
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Projects"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+// src/App.jsx
+// src/App.tsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+
+const App: React.FC = () => {
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Projects"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+// src/components/Header.jsx
+import React from 'react';
+import Navigation from './Navigation';
+
+const Header: React.FC = () => {
+    return (
+      <header>
+        <h1>Your Portfolio Name</h1>
+        <Navigation />
+      </header>
+    );
+  };
+  
+  export default Header;
