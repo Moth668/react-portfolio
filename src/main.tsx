563a2e287d44d2e6e9cf16b5e3c8082bc4aba2a1
Moth <tdrice668@gmail.com>
2024-11-02 18:55:47 -0500

    fixed main.tsx errors causing deploy failure

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './App'
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
-// src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import './index.css';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
