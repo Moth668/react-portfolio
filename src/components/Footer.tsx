9eea98f9d09180662c489f2ad4d7e1b226fd803c
Moth <tdrice668@gmail.com>
2024-11-02 18:29:47 -0500

    rearranged footer, changed contact page message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
       <footer>
-        <p>© 2024 Your Name. All Rights Reserved.</p>
-        <ul>
+                <ul>
           <li><a href="https://github.com/Moth668">GitHub</a></li>
           {/* <li><a href="https://linkedin.com/in/yourusername">LinkedIn</a></li> */}
           <li><a href="mailto:tdrice668@gmail.com">Email</a></li>
         </ul>
+        <p>© 2024 Your Name. All Rights Reserved.</p>
       </footer>
     );
   };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ function Message() {
     // JSX: JavaScript XML
     const name = 'Tim';
     if (name)
-    return <h1>Hello {name}</h1>;
+    return <h1>Hello, I'm {name}. Send me a message.</h1>;
     return <h1>Hello World</h1>;
 }
 
