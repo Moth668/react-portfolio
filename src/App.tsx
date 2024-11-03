c4cd308e4538b6142894ec5693560a52e6544f89
Moth <tdrice668@gmail.com>
2024-11-02 23:11:06 -0500

    adjustments to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 // src/App.tsx
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import Weather from "./pages/Weather";
-import { useTheme } from "./context/ThemeContext";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useTheme } from './context/ThemeContext';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Weather from './pages/Weather';
 
 const App: React.FC = () => {
   const { theme } = useTheme();
 
   return (
     <Router>
-      <div className="d-flex flex-column min-vh-100">
       <div
-        className={
-          theme === "dark" ? "bg-dark text-white" : "bg-light text-dark"
-        }
+        className={`d-flex flex-column min-vh-100 ${
+          theme === 'dark' ? 'bg-dark text-white' : 'bg-light text-dark'
+        }`}
       >
         <Header />
         <main className="flex-grow-1">
@@ -32,7 +31,6 @@ const App: React.FC = () => {
           </Routes>
         </main>
         <Footer />
-      </div>
       </div>
     </Router>
   );
