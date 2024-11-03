73650658925086cc5f5f875cdd90c74757456982
Moth <tdrice668@gmail.com>
2024-11-02 21:52:50 -0500

    added geocoding to weather app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config(); // Load environment variables
+
 export default defineConfig({
   plugins: [react()],
-})
+});
