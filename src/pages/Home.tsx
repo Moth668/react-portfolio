10c877bf39a857aafd4d197aae31f235a4b4ece2
Moth <tdrice668@gmail.com>
2025-02-09 13:14:23 -0600

    added fontawesome npm package and brand icons for footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,22 +11,22 @@ const Home: React.FC = () => {
       <h1 className="display-1 text-center mb-4">Timothy Rice</h1>
       <p className="lead text-center">
         Hi, I am Tim Rice — a software developer driven by a love for crafting
-        dynamic and efficient applications. Focused on full-stack development, I
+        dynamic and efficient applications. Focused on MERN full-stack development, I
         thrive on the challenge of learning and integrating the latest
         technologies to deliver innovative solutions.
       </p>
       <p className="lead text-center">
-        With a strong foundation in JavaScript, React, and TypeScript, I have
+        With a strong foundation in React, and TypeScript, I have
         developed and contributed to various projects that emphasize clean,
         maintainable, and high-performing code. Problem-solving is at the core
         of what I do, and I enjoy transforming complex ideas into user-friendly
         digital experiences.
       </p>
       <p className="lead text-center">
-        I am currently enrolled in the Washington University in St. Louis EdX
-        programming bootcamp where I am enhancing my skills in full-stack web
+        I have graduated the Washington University in St. Louis EdX
+        programming bootcamp where I enhanced my skills in full-stack web
         development, learning industry best practices, and collaborating with
-        fellow aspiring developers. This intensive program has been instrumental
+        fellow aspiring developers. This intensive program was instrumental
         in expanding my knowledge and preparing me for the world of technology.
       </p>
       <p className="lead text-center">
@@ -41,7 +41,7 @@ const Home: React.FC = () => {
         reach out to me if you would like to connect or collaborate.
       </p>
       <p className="text" id="machine">
-        I sourced parts and built my machine myself at home. Here's some specs:
+        I sourced all individual parts, and built my machine myself at home. Here's some specs:
         <ul>
           <li>Motherboard: MSI Tomahawk B550</li>
           <li>CPU: AMD Ryzen 5 3600 6-Core Processor 3.60 GHz</li>
@@ -53,36 +53,41 @@ const Home: React.FC = () => {
           <li>Mouse: Corsair Harpoon RGB</li>
         </ul>
       </p>
-      My list of skills includes, (but is not limited to) these software tools:
+      My list of skills includes, but is not limited to, these software tools:
       <p className="text" id="skills">
         <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
+          <li>Visual Studio Code</li>
+          <li>GitHub</li>
+          <li>React</li>
           <li>TypeScript</li>
-          <li>Python</li>
           <li>Node.js</li>
           <li>Express.js</li>
-          <li>React</li>
           <li>React Developer Tools</li>
-          <li>Bootstrap</li>
           <li>MongoDB</li>
+          <li>Chakra UI</li>
+          <li>Bootstrap</li>
           <li>Mongoose</li>
-          <li>RESTful APIs</li>
           <li>GraphQL</li>
+          <li>Insomnia</li>
+          <li>HTML5</li>
           <li>GitBash</li>
-          <li>GitHub</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+          <li>Python</li>
+          <li>RESTful APIs</li>
           <li>OpenAI</li>
           <li>Sequelize</li>
           <li>PostgreSQL</li>
-          <li>Insomnia</li>
           <li>Netlify</li>
           <li>Render</li>
-          <li>Visual Studio Code</li>
           <li>Microsoft Office Suite</li>
           <li>Google Workspace</li>
           <li>Zoom</li>
           <li>Slack</li>
+          <li>Discord</li>
+          <li>Godot</li>
+          <li>Render</li>
+          <li>ProCreate</li>
         </ul>
       </p>
     </div>
